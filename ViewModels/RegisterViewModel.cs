b7fa92a9172c859e53e18d130b06fe2913389125
Teocelik <aalihan@outlook.com.tr>
2024-12-08 19:08:59 +0300

    - Kullanıcı kayıt ve girişi için viewModel oluşturuldu.

--- ViewModels/RegisterViewModel.cs
+++ ViewModels/RegisterViewModel.cs
@@ -1,6 +1,24 @@
-﻿namespace UserManagmentModule.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagmentModule.ViewModels
 {
     public class RegisterViewModel
     {
+        [Required(ErrorMessage = "Kullanıcı adını giriniz!")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "E-mail adresini giriniz!")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "şifreyi giriniz!")]
+        [MinLength(6, ErrorMessage = "Parola 6 karakterden kısa olmamalıdır!")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$",
+        ErrorMessage = "Prolanız, küçük harf, büyük harf ve sayı içermelidir.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "şifreyi tekrar giriniz!")]
+        [Compare("Password", ErrorMessage = "Şifre eşleşmedi!.")]
+        public string ConfirmPassword { get; set; }
     }
 }
